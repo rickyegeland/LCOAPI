d4abb93aefdc8a9a7b09d9bc2621f3e5eb53725b
Ricky Egeland <ricky.egeland@gmail.com>
2020-07-02 16:27:52 -0600

    Add new PROPOSAL_ID

--- submit_head.py
+++ submit_head.py
@@ -3,10 +3,10 @@ import requests
 
 API_TOKEN = ‘PASTE_YOUR_API_TOKEN_HERE’
 #PROPOSAL_ID = 'LCO2020A-016'
-PROPOSAL_ID = 'NOAO2020A-002'
+#PROPOSAL_ID = 'NOAO2020A-002'
+PROPOSAL_ID = 'KEY2020B-010'
 
 windows = [{
-    'start': '2020-05-16 00:00:00',
-    'end': '2020-05-29 23:59:59'
+    'start': '2020-07-01 00:00:00',
+    'end': '2020-07-12 23:59:59'
 }]
-
